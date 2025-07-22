f94cfaec014a9e63f6521f740ded38170147a1b9
Babajan GeldimuradoW <f.gump248651937@gmail.com>
2025-07-23 00:15:01 +0500

    chan icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>{{ __('messages.app_title') }}</title>
+     <link rel="icon" href="{{ asset('icon.png') }}" type="image/icon" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
